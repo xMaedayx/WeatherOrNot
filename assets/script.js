bbe3fc2b497ae071d00f71744556c87dbfa7277a
xMaedayx <120540407+xMaedayx@users.noreply.github.com>
2023-02-06 11:56:29 -0700

    updates

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var inputval = document.querySelector('#cityinput')
+var btn = document.querySelector('#add');
+var city = document.querySelector('#cityoutput')
+var descrip = document.querySelector('#description')
+var temp = document.querySelector('#temp')
+var wind = document.querySelector('#wind')
+var humid = document.querySelector('#humidity')
+var dewPoint = document.querySelector('#dp')
+
